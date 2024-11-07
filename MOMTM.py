0a8cc71622867c5ab1fd16f9ba48e331410a52b9
Haibo_Shen <154346047+shb289791170@users.noreply.github.com>
2024-11-07 09:49:32 +0800

    MOMTM.py

--- MOMTM.py
+++ MOMTM.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.metrics import pairwise_distances
 from scipy.spatial.distance import braycurtis
-from tqdm import tqdm  # 导入tqdm库
+from tqdm import tqdm  
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
@@ -30,43 +30,32 @@ otu_data_path = 'your_data_file.csv'
 metadata = pd.read_csv(metadata_path, encoding='GBK')
 otu_data = pd.read_csv(otu_data_path, encoding='GBK')
 
-# 重命名otu_data中的样本ID列，以便与metadata中的Sample_ID列匹配
 otu_data.rename(columns={'Unnamed: 0': 'sample_ID'}, inplace=True)
 #otu_data.iloc[:, 1:] = otu_data.iloc[:, 1:].div(otu_data.iloc[:, 1:].sum(axis=1), axis=0)
 combined_data = pd.merge(metadata, otu_data, on='sample_ID', how='left')
 combined_data = combined_data[combined_data['body_Site'] == 'Stool']
 original_data = combined_data.copy()
-#将样本信息转换为行名，利于后面的数据处理
 def combine_sample_info(row):
     return '|'.join(map(str, row))
 
 combined_data['sample_info'] = combined_data.iloc[:, :11].apply(combine_sample_info, axis=1)
-# 将'sample_info'列设置为索引
 combined_data.set_index('sample_info', inplace=True)
 
 combined_data = combined_data[combined_data['body_Site'] == 'Stool']
-# 删除前7列样本信息
 combined_data = combined_data.iloc[:, 11:]
 
 
 def filter_and_replace_zeros(df, threshold=0.5):
-    # 计算每个特征中零值的比例
     zero_proportion = (df == 0).mean()
-    # 保留零值比例小于阈值的特征
     filtered_df = df.loc[:, zero_proportion < threshold]
-    # 确保数据类型为浮点数
     filtered_df = filtered_df.astype(float)
-    # 将NA值替换为零
     filtered_df = filtered_df.fillna(0)
-    # 将pandas数据帧转换为R数据帧
     r_df = pandas2ri.py2rpy(filtered_df)
-    # 使用zCompositions包进行零替换
     r_result = zCompositions.cmultRepl(r_df, label=0, method="CZM")
-    # 将结果转换回pandas数据帧
     replaced_df = pandas2ri.rpy2py(r_result)
     return replaced_df
 combined_data_replaced = filter_and_replace_zeros(combined_data)
-#这部分为了feature移除的时候，运行filter_and_replace_zeros后features数量一致
+
 combined_data_replaced_features = combined_data_replaced.copy()
 combined_data_replaced_features['sample_ID'] = combined_data_replaced_features.index.str.split('|').str[0]
 combined_data_replaced_features['stage'] = combined_data_replaced_features.index.str.split('|').str[1]
@@ -102,46 +91,29 @@ sow_data = combined_data_clr[combined_data_clr['generations'] == 'sow']
 sow_data = sow_data.dropna(subset=sow_data.columns[10:])
 sow_data_features = combined_data_replaced_features[combined_data_replaced_features["generations"] == 'sow']
 sow_data_features = sow_data_features.dropna(subset=sow_data_features.columns[11:])
-#print( np.unique(sow_data["stage"])) ['G85''G100''G112''S_D1''S_D3''S_D7''S_D14''S_D21'
- #  ]
+
 piglet_data = combined_data_clr[combined_data_clr['generations'] == 'piglet']
 piglet_data_features = combined_data_replaced_features[combined_data_replaced_features["generations"] == 'piglet']
 piglet_data = piglet_data.dropna(subset=piglet_data.columns[10:])
 piglet_data_features = piglet_data_features.dropna(subset=piglet_data_features.columns[10:])
-#分割piglet_data为两个部分
-# 从 'stage' 列提取天数，使用 raw string notation 避免 invalid escape sequence
+
 #piglet_data['day'] = piglet_data['stage'].str.extract(r'D(\d+)').astype(int)
 piglet_data['day'] = pd.to_numeric(piglet_data['day'], errors='coerce')
 piglet_data_features['day'] = pd.to_numeric(piglet_data_features['day'], errors='coerce')
 sow_data['day'] = pd.to_numeric(sow_data['day'], errors='coerce')
 sow_data_features['day'] = pd.to_numeric(sow_data_features['day'], errors='coerce')
-# 根据天数分割数据为 sucking 和 weaned
-filter_train_data = piglet_data[piglet_data['group_detail'].isin(['Local', 'Commercial'])]
-filter_train_data2  = piglet_data[~piglet_data['group_detail'].isin(['Local', 'Commercial'])]
-unique_NX_pair_ids = filter_train_data2['pair_ID'].unique()
-train_NX_pair_ids, test_NX_pair_ids = train_test_split(unique_NX_pair_ids, test_size=0.1, random_state=42)
-train_NX_data = filter_train_data2[filter_train_data2['pair_ID'].isin(train_NX_pair_ids)]
-filter_train_data = pd.concat([train_NX_data, filter_train_data], ignore_index=True)
-#filter_train_data = piglet_data
-filter_train_data_features = piglet_data_features[piglet_data_features['group_detail'].isin(['Local', 'Commercial'])]
-filter_train_data_features2 = piglet_data_features[~piglet_data_features['group_detail'].isin(['Local', 'Commercial'])]
-train_NX_data_feature = filter_train_data_features2[filter_train_data_features2['pair_ID'].isin(train_NX_pair_ids)]
-filter_train_data_features = pd.concat([train_NX_data_feature, filter_train_data_features], ignore_index=True)
-#filter_train_data_features = piglet_data_features
+# seperate data to sucking and weaned stage
+filter_train_data = piglet_data
+filter_train_data_features = piglet_data_features
 sucking_data = filter_train_data[filter_train_data['day'] <= 21]
 sucking_data_features =filter_train_data_features[filter_train_data_features['day'] <= 21]
 
-
-#print( np.unique(sucking_data["stage"]))
-#['D1' 'D3' 'D7''D10' 'D14' 'D18' 'D21' 'D28' ]
 weaned_data = piglet_data[piglet_data['day'] > 21]
 weaned_data_features =piglet_data_features[piglet_data_features['day'] > 21]
-#print( np.unique(weaned_data["stage"]))
-#['D35''D49''D63''D70''D90''D120''D150']
-#计算哺乳期：
+#calculate sucking stage
 unique_sucking_pair_ids = sucking_data_features['pair_ID'].unique()
 train_pair_ids, test_pair_ids = train_test_split(unique_sucking_pair_ids, test_size=0.3, random_state=42)
-# 根据 pair_IDs 分割 sow_data 和 piglet_data
+
 train_sucking_data = sucking_data[sucking_data['pair_ID'].isin(train_pair_ids)]
 test_sucking_data = sucking_data[sucking_data['pair_ID'].isin(test_pair_ids)]
 
@@ -404,14 +376,12 @@ for epoch in range(num_epochs):
 
         encoder_input = sow_data1.view(batch_size, time_steps, feature_size)
 
-        # 直接预测所有时间点的数据
         outputs = model(encoder_input)
         loss = loss_fn(outputs, piglet_data_masked, masks1, model)
 
         loss.backward()
         optimizer.step()
 
-        # 应用非负权重约束
         enforce_non_negative_weights(model)
 
         train_loss += loss.item()
@@ -476,7 +446,7 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-#model, optimizer, epoch, loss = load_checkpoint(model, optimizer, 'H:/新建文件夹/25.博士论文参考文献/博士毕业数据/4.15.2024数据处理/attention+weight_loss_postive_rate.pt')
+#model, optimizer, epoch, loss = load_checkpoint(model, optimizer, '*.pt')
 
 
 def plot_with_fit(x, y, xlabel, ylabel, title, color, label):
@@ -504,7 +474,7 @@ def evaluate_model(dataloader, model, device):
     model.eval()
     mse_scores = []
     mae_scores = []
-    r2_scores = []  # 初始化 R² 分数列表
+    r2_scores = [] 
     all_true_values = []
     all_predicted_values = []
 
@@ -517,7 +487,6 @@ def evaluate_model(dataloader, model, device):
             piglet_data_masked = piglet_data * masks2
             batch_size, seq_length, feature_size = sow_data1.shape
             encoder_input = sow_data1.view(batch_size, seq_length, feature_size)
-            # 直接预测所有时间点的数据
             outputs = model(encoder_input) * masks1
             true_values = piglet_data_masked.view(-1, feature_size).detach().cpu().numpy()
             predicted_values = outputs.view(-1, feature_size).detach().cpu().numpy()
@@ -531,16 +500,15 @@ def evaluate_model(dataloader, model, device):
 
                 mse = mean_squared_error(true_feature_values, predicted_feature_values)
                 mae = mean_absolute_error(true_feature_values, predicted_feature_values)
-                r2 = r2_score(true_feature_values, predicted_feature_values)  # 计算 R² 值
+                r2 = r2_score(true_feature_values, predicted_feature_values)  
 
                 mse_scores.append(mse)
                 mae_scores.append(mae)
-                r2_scores.append(r2)  # 添加 R² 分数到列表
+                r2_scores.append(r2)  
 
     all_true_values = np.vstack(all_true_values)
     all_predicted_values = np.vstack(all_predicted_values)
 
-    # 使用PCA进行降维
     pca = PCA(n_components=2)
     all_data = np.vstack([all_true_values, all_predicted_values])
     pca_transformed = pca.fit_transform(all_data)
@@ -573,7 +541,6 @@ def evaluate_model(dataloader, model, device):
     return avg_mse, avg_mae, avg_r2
 
 
-# 示例用法，确保 dataloader_test, model, device 已定义
 mse, mae, r2 = evaluate_model(dataloader_test, model, device)
 print(f"Test Average MSE: {mse}, Test Average MAE: {mae}, Test Average R²: {r2}")
 
@@ -594,7 +561,6 @@ def preprocess_data(data, removed_feature):
     modified_data = data.copy()
     if removed_feature in modified_data.columns:
         modified_data[removed_feature] = 0
-    # 归一化数据
     modified_data = modified_data.div(modified_data.sum(axis=1), axis=0)
     return modified_data
 
@@ -639,7 +605,6 @@ def get_tensors(sow_data, piglet_data, mask_df, stages, feature_names):
                         sow_tensor_after = torch.tensor(modified_sow_data_clr.values, dtype=torch.float32)
                         sow_tensors_after[(pig_id, stage, removed_feature)] = sow_tensor_after
 
-                        # 存储相对丰度
                         relative_abundances[(pig_id, stage, removed_feature)] = relative_abundance
 
     return piglet_tensors, sow_tensors_before, sow_tensors_after, mask_tensor, relative_abundances
@@ -648,12 +613,10 @@ piglet_tensors, sow_tensors_before, sow_tensors_after, mask_tensor,relative_abun
                                                                                                      masks_features, sucking_stages, feature_names)
 
 def euclidean_distance(piglet_tensor_batch, pred_batch):
-    # 计算欧式距离
     diff = piglet_tensor_batch - pred_batch
-    dist_squared = torch.sum(diff ** 2, dim=1)  # 沿最后一个维度求和
+    dist_squared = torch.sum(diff ** 2, dim=1) 
     euclidean_dist = torch.sqrt(dist_squared)
 
-    # 返回批次的平均欧式距离
     return euclidean_dist.mean().item()
 
 
@@ -661,12 +624,10 @@ def calculate_transmission_coreness(model, piglet_tensors, sow_tensors_before, s
                                     relative_abundances, feature_names, stages):
     model.eval()
     bc_distances = []
-    device = next(model.parameters()).device  # 获取模型的设备
+    device = next(model.parameters()).device 
     with torch.no_grad():
-        # 按照 pig_id 进行分组
         pig_ids = set(pig_id for pig_id, _ in piglet_tensors.keys())
         for pig_id in tqdm(pig_ids, desc="Calculating coreness"):
-            # 获取该 pig_id 对应的所有阶段的 piglet_tensor 和 sow_tensors_before
             try:
                 piglet_tensors_for_pig = [piglet_tensors[(pig_id, stage)].to(device) for stage in stages]
                 sow_tensors_before_for_pig = [sow_tensors_before[(pig_id, stage)].to(device) for stage in stages]
@@ -674,7 +635,6 @@ def calculate_transmission_coreness(model, piglet_tensors, sow_tensors_before, s
                 print(f"KeyError: {e} - missing data for pig_id: {pig_id} in stages")
                 continue
             for removed_feature in feature_names:
-                # 获取该 pig_id 对应的所有阶段的 sow_tensors_after
                 sow_tensors_after_for_pig = []
                 valid = True
                 for stage in stages:
@@ -685,24 +645,19 @@ def calculate_transmission_coreness(model, piglet_tensors, sow_tensors_before, s
                         break
                 if not valid:
                     continue
-                # 转换为张量
                 sow_tensor_before_batch = torch.stack(sow_tensors_before_for_pig)
                 sow_tensor_after_batch = torch.stack(sow_tensors_after_for_pig)
                 piglet_tensor_batch = torch.stack(piglet_tensors_for_pig)
                 sow_tensor_before_batch = sow_tensor_before_batch.squeeze(1)
                 sow_tensor_after_batch = sow_tensor_after_batch.squeeze(1)
                 piglet_tensor_batch = piglet_tensor_batch.squeeze(1)
-                # 模型预测
                 combined_batch = torch.cat((sow_tensor_before_batch.unsqueeze(0), sow_tensor_after_batch.unsqueeze(0)),
                                            dim=0)
-                # 打印检查
-                # 计算欧式距离
                 preds = model(combined_batch)
                 pred_before, pred_after = preds[0], preds[1]
                 dist_before = euclidean_distance(piglet_tensor_batch, pred_before)
                 dist_after = euclidean_distance(piglet_tensor_batch, pred_after)
-                # 获取相对丰度并计算差异
-                for stage in stages:
+                # 获取相对丰度并计算差异                for stage in stages:
                     if (pig_id, stage, removed_feature) in relative_abundances:
                         relative_abundance = relative_abundances[(pig_id, stage, removed_feature)]
                         dist_diff = abs(dist_after - dist_before)
@@ -721,8 +676,6 @@ def calculate_transmission_coreness(model, piglet_tensors, sow_tensors_before, s
 
 
 
-
-# 确保传递的所有参数
 bc_distances = calculate_transmission_coreness(
     model,
     piglet_tensors,
@@ -733,23 +686,22 @@ bc_distances = calculate_transmission_coreness(
     sucking_stages
 )
 df = pd.DataFrame(bc_distances)
-#用掩码去掉多余的数据
+
 masks_features.set_index('pig_ID', inplace=True)
 #filled_sucking_data_features, masks_features = fill_and_mask(piglet_data_features, sucking_stages)
 
-# 定义一个函数来检查是否应该保留一行
+
 def should_keep_row(row):
     pig_id = row['pig_id']
     stage = row['stage']
     if pig_id in masks_features.index and stage in masks_features.columns:
         return masks_features.at[pig_id, stage]
-    return True  # 如果没有匹配的掩码信息，则保留该行
+    return True  
 
-# 过滤 df 数据集
 filtered_df = df[df.apply(should_keep_row, axis=1)]
 
 metadata1 =metadata.copy()
 metadata1.rename(columns={'pig_ID': 'pig_id'}, inplace=True)
 merged_df = pd.merge(filtered_df, metadata1[['pig_id', 'Breed']], on='pig_id', how='left')
-# 删除 pig_id、stage 和 removed_feature 都相同的重复行，只保留第一次出现的行
+
 merged_df = merged_df.drop_duplicates(subset=['pig_id', 'stage', 'removed_feature'])
